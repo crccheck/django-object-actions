b4b11914cf5ea8d5e136d5ed7f0e85f152184e50
crccheck <c@crccheck.com>
2014-09-02 21:16:26 -0500

    add coverage to the project

--- django_object_actions/factories.py
+++ django_object_actions/factories.py
@@ -1,6 +1,6 @@
 try:
     from django.contrib.auth import get_user_model
-except ImportError:
+except ImportError:  # pragma: no cover
     # Django 1.4
     from django.contrib.auth.models import User
     get_user_model = lambda: User
