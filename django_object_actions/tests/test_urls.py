de6113ce3d8d5f2fca5b3efe9b446615c77737f4
crccheck <c@crccheck.com>
2019-10-06 16:41:12 -0500

    black

--- django_object_actions/tests/test_urls.py
+++ django_object_actions/tests/test_urls.py
@@ -11,6 +11,8 @@ from .tests import LoggedInTestCase
 
 class ChangeListTests(LoggedInTestCase):
     def test_changelist_action_is_rendered(self):
-        response = self.client.get(reverse('admin:polls_choice_changelist'))
+        response = self.client.get(reverse("admin:polls_choice_changelist"))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'href="/admin/polls/choice/actions/delete_all/"', response.content)
+        self.assertIn(
+            b'href="/admin/polls/choice/actions/delete_all/"', response.content
+        )
