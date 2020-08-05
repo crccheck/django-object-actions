74cacb346f0af0ecc82fde5d194ad163f3fd4999
crccheck <c@crccheck.com>
2020-08-04 23:47:42 -0500

    drop py3.4, add py3.8

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
