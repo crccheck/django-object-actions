4340c8958492dfd2e6113fcba6fdcdc90be02e8e
Bruno Alla <browniebroke@users.noreply.github.com>
2021-11-08 06:44:15 +0000

    Migrate CI to GitHub actions (#122)
    TravisCI stopped building 7 months ago, migrate to GitHub actions which seems to be the default choice these days. Some comments about this change:
    
    - I tried to keep the build matrix close to how it was but [the build failed](https://github.com/browniebroke/django-object-actions/actions/runs/1431603631) for Django<2, so I've dropped these versions. 
    - Python 3.5 reached the end of its life on September 13th, 2020, so I've dropped it as well.
    - The build status isn't reported in this PR because it's a new workflow added from a fork, and GitHub doesn't run it for security reasons. You can see the [build results in my fork](https://github.com/browniebroke/django-object-actions/actions).
    - I didn't bother trying to add new versions for now, can be done separately.

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
